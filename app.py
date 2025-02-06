3d9c9f3ad52a0533f7f70d5224485eb2ac0bf128
Giovanni Bocchi <giovanni.bocchi@gmail.com>
2025-02-06 11:09:54 +0100

    cpapello replaced

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from streamlit_cropperjs import st_cropperjs
 from PIL import Image, ImageDraw, ImageFont
 import io
 import os
+import base64
 
 from wantedposter.wantedposter import (
     WantedPoster,
@@ -12,6 +13,205 @@ from wantedposter.wantedposter import (
     Stamp,
     Effect,
 )
+
+@st.cache_data(ttl=3600)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def support_icon(
+        local_img_path_button,
+
+        local_img_path_telegram,
+        target_url_telegram,
+
+        local_img_path_wahtsapp,
+        target_url_whatsapp,
+
+        local_img_path_mail,
+        target_url_mail
+):
+    img_format_button = os.path.splitext(local_img_path_button)[-1].replace('.', '')
+    bin_str_button = get_base64_of_bin_file(local_img_path_button)
+
+    img_format_telegram = os.path.splitext(local_img_path_telegram)[-1].replace('.', '')
+    bin_str_telegram = get_base64_of_bin_file(local_img_path_telegram)
+    img_format_whatsapp = os.path.splitext(local_img_path_wahtsapp)[-1].replace('.', '')
+    bin_str_whatsapp = get_base64_of_bin_file(local_img_path_wahtsapp)
+    img_format_mail = os.path.splitext(local_img_path_mail)[-1].replace('.', '')
+    bin_str_mail = get_base64_of_bin_file(local_img_path_mail)
+
+    html_code = f'''
+            <style>
+                #menuToggle {{
+                      display: block;
+                      position: fixed;
+                      bottom:30px;
+                      left:30px;
+                      z-index: 1;
+                      -webkit-user-select: none;
+                      user-select: none;
+                }}
+                # @media (orientation: portrait) {{
+                #         #menuToggle {{
+                #             bottom:100px;
+                #         }}
+                # }}
+                # @supports (-webkit-appearance: none) and (not (overflow:-webkit-marquee)) {{
+                #         #menuToggle {{
+                #             bottom:97px;
+                #         }}
+                # }}
+                #menuToggle a {{
+                    text-decoration: none;
+                    color: white;
+                    transition: color 0.3s ease;
+                }}
+                #menuToggle a:hover {{
+                    color: rgb(236,194,12);
+                }}
+                #menuToggle input {{
+                    display: block;
+                    width: 65px;
+                    height: 65px;
+                    position: absolute;
+                    top: -7px;
+                    left: -5px;
+                    cursor: pointer;
+                    opacity: 0; /* hide this */
+                    z-index: 2; /* and place it over the hamburger */
+                    -webkit-touch-callout: none;
+                }}
+                #menuToggle .float2 {{
+                    display: block;
+                    width: 33px;
+                    height: 4px;
+                    margin-bottom: 5px;
+                    position: relative;
+                    background: rgb(26,34,44);
+                    border-radius: 3px;
+                    z-index: 1;
+                    transform-origin: 4px 0px;
+                    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+                              background 0.5s cubic-bezier(0.77,0.2,0.05,1.0),
+                              opacity 0.55s ease;
+                }}
+                #menuToggle .float:first-child {{
+                    transform-origin: 0% 0%;
+                }}
+                #menuToggle .float:nth-last-child(2) {{
+                    transform-origin: 0% 100%;
+                }}
+                #menuToggle input:checked ~ span {{
+                    opacity: 1;
+                    transform: rotate(45deg) translate(-2px, -1px);
+                    background: white;
+                }}
+                #menuToggle input:checked ~ span:nth-last-child(3) {{
+                    opacity: 0;
+                    transform: rotate(0deg) scale(0.2, 0.2);
+                }}
+                #menuToggle input:checked ~ span:nth-last-child(2) {{
+                    transform: rotate(-45deg) translate(0, -1px);
+                }}
+                #menu {{
+                display: flex;
+                    flex-direction: row;
+                    justify-content: flex-end;
+                    align-items: center;
+                    border-bottom-right-radius: 55px;
+                    border-top-right-radius: 55px;
+                    position: absolute;
+                    width: 400px;
+                    margin: -90px 0 0 -50px;
+                    padding: 20px;
+                    # padding-top: 125px;
+                    background: rgb(26,34,44);
+                    list-style-type: none;
+                    -webkit-font-smoothing: antialiased;
+                    /* to stop flickering of text in safari */
+                    transform-origin: 0% 0%;
+                    transform: translate(-100%, 0);
+                    transition: transform 0.5s cubic-bezier(0.77,0.2,0.05,1.0);
+                    }}
+
+                #menu li
+                {{
+                    padding: 10px 0;
+                    font-size: 22px;
+                }}
+                #menuToggle input:checked ~ ul {{
+                    transform: none;
+                }}
+
+                .box_keywords {{
+                    display: none;
+                }}
+
+                #menuToggle .float {{
+                    # position:fixed;
+                    width:65px;
+                    height:65px;
+                    # bottom:20px;
+                    # left:20px;
+                    background-color:#2d78c9;
+                    color:#FFF;
+                    border-radius:50px;
+                    text-align:center;
+                    box-shadow: 2px 2px 3px rgb(0 0 0 / 0.2);
+                    # transition: transform .2s; /* Animation */
+                    margin-left: 20px;
+                }}
+
+                #menuToggle .float2 {{
+                    # position:fixed;
+                    width:65px;
+                    height:65px;
+                    # bottom:20px;
+                    # left:20px;
+                    background-color:#2d78c9;
+                    color:#FFF;
+                    border-radius:50px;
+                    text-align:center;
+                    box-shadow: 0px 0px 14px rgb(0 0 0 / 0.92);
+                    # transition: transform .2s; /* Animation */
+                }}
+
+                .float:hover {{
+                    background-color:#ffffff;
+                }}
+
+                .float img {{
+                    width: 55px;
+                    height: 55px;
+                    margin-top:5px;
+                }}
+            </style>
+
+
+            <nav role="navigation">
+                <div id="menuToggle">
+                    <input type="checkbox" />
+                        <img src="data:image/{img_format_button};base64,{bin_str_button}" class="float2"/>
+                        <ul id="menu">
+                                <a href="https://t.me/thegiova" target="_blank" class="float">
+                                <img src="data:image/{img_format_telegram};base64,{bin_str_telegram}" />
+                                </a>
+                                <a href="https://api.whatsapp.com/send?phone=3494346732&text=Ciao,%20sono%20interessato%20ai%20vostri%20prodotti." target="_blank" class="float">
+                                <img src="data:image/{img_format_whatsapp};base64,{bin_str_whatsapp}" />
+                                </a>
+                                <a href="{target_url_mail}" class="float">
+                                <img src="data:image/{img_format_mail};base64,{bin_str_mail}" />
+                                </a>
+                        </ul>
+                </div>
+            </nav>
+
+            '''
+    return html_code
+
+
 
 # --- page configuration ---
 st.set_page_config(
@@ -29,7 +229,7 @@ st.set_page_config(
 
 st.write('')
 st.write('')
-st.image('assets/cappello.png')
+st.image('static/cappello.png')
 st.write('')
 
 # --- contact ---
@@ -281,3 +481,15 @@ if file_uploaded:
             )
         except:
             st.info('Confirm to generate.')
+
+html_code = support_icon(
+    "static/support.png",
+    "static/telegram.png",
+    "mailto:giovanni.bocchi@gmail.com?subject=One Piece Wanted - Supporto&body=Ciao Supporto!",
+    "static/whatsapp.png",
+    "mailto:mastro.costruttore@gmail.com?subject=One Piece Wanted - Supporto&body=Ciao Supporto!",
+    "static/mail.png",
+    "mailto:giovanni.bocchi@gmail.com?subject=One Piece Wanted - Supporto&body=Ciao Supporto!"
+)
+
+st.markdown(html_code, unsafe_allow_html=True)
